12880fdeb1823fb4841e691f8cf567a12dd56110
liyongzhi02 <liyongzhi02@58.com>
2024-04-25 15:57:50 +0800

    feat: 引入组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <RouterView>
-    <el-config-provider :locale="locale.value">
+    <el-config-provider :locale="locale">
       <el-button type="primary">按钮</el-button>
     </el-config-provider>
   </RouterView>
@@ -10,7 +10,7 @@
 import { defineProps, ref } from 'vue'
 import { ElConfigProvider } from 'element-plus'
 
-import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const props = defineProps({ locale: zhCn })
 
