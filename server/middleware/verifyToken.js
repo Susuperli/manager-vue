62d0865cc7dfd1c2e580649704d7830258180278
liyongzhi02 <liyongzhi02@58.com>
2024-06-19 10:54:50 +0800

    feat: 验证token中间价优化

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -3,23 +3,21 @@ const { jumpTokenList, USER_ID } = require('./../constance')
 
 const verifyTokenMiddleware = () => {
   return (req, res, next) => {
-    if (!jumpTokenList.includes(req.path)) {
-      const token = req.headers.authorization
-      const isValid = verifyToken(token ?? '')
+    const token = req.headers.authorization
 
-      const userId = req.cookies?.[USER_ID]
+    const isValid = verifyToken(token ?? '')
+    const userId = req.cookies?.[USER_ID]
 
-      if (!isValid || !userId) {
-        res.status(401).send({
-          code: 401,
-          msg: 'token失效，请重新登录',
-          data: null,
-          success: false
-        })
-      }
+    if (!jumpTokenList.includes(req.path) && (!isValid || !userId)) {
+      res.status(401).send({
+        code: 401,
+        msg: 'token失效，请重新登录',
+        data: null,
+        success: false
+      })
+    } else {
+      next()
     }
-
-    next()
   }
 }
 
