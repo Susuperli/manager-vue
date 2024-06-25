84d5225b4d535e4c26bbb6839443446fdc435ed0
liyongzhi02 <liyongzhi02@58.com>
2024-06-25 17:51:09 +0800

    feat: token允许为空

--- server/db/models/Token.js
+++ server/db/models/Token.js
@@ -3,7 +3,7 @@ const mongoose = require('../index')
 const tokenSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    token: { type: String, required: true, unique: true }
+    token: { type: String, required: true }
   },
   { timestamps: true }
 )
